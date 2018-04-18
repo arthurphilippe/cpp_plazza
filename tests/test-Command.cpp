49bae4b0ae99b90d7159b33da41c3b525c8432a4
Arthur C. Philippe <arthur.philippe@epitech.eu>
2018-04-18 10:51:50 +0200

    [#4] more tests

--- tests/test-Command.cpp
+++ tests/test-Command.cpp
@@ -28,10 +28,36 @@ Test(Command, serialasation_basic) {
 	cr_assert_eq(cmd1.cmdInfoType, cmd2.cmdInfoType);
 }
 
-Test(Command, serialasation_basic) {
+Test(Command, serialasation_double) {
 	Plazza::Command cmd1;
 
 	cmd1.cmdFileName = "toto";
+	cmd1.cmdId = 42;
+	cmd1.cmdInfoType = Plazza::Information::EMAIL_ADDRESS;
+
+	std::stringstream ioss;
+	ioss << cmd1;
+	ioss << cmd1;
+
+	Plazza::Command cmd2;
+	ioss >> cmd2;
+	cr_assert_eq(cmd1.cmdId, cmd2.cmdId);
+	cr_assert_str_eq(cmd1.cmdFileName.c_str(), cmd2.cmdFileName.c_str());
+	cr_assert_str_eq(cmd1.cmdFileName.c_str(), cmd2.cmdFileName.c_str());
+	cr_assert_eq(cmd1.cmdInfoType, cmd2.cmdInfoType);
+
+	Plazza::Command cmd3;
+	ioss >> cmd3;
+	cr_assert_eq(cmd1.cmdId, cmd3.cmdId);
+	cr_assert_str_eq(cmd1.cmdFileName.c_str(), cmd3.cmdFileName.c_str());
+	cr_assert_str_eq(cmd1.cmdFileName.c_str(), cmd3.cmdFileName.c_str());
+	cr_assert_eq(cmd1.cmdInfoType, cmd3.cmdInfoType);
+}
+
+Test(Command, serialasation_empty_name) {
+	Plazza::Command cmd1;
+
+	cmd1.cmdFileName = "";
 	cmd1.cmdId = 42;
 	cmd1.cmdInfoType = Plazza::Information::EMAIL_ADDRESS;
 
@@ -45,3 +71,22 @@ Test(Command, serialasation_basic) {
 	cr_assert_str_eq(cmd1.cmdFileName.c_str(), cmd2.cmdFileName.c_str());
 	cr_assert_eq(cmd1.cmdInfoType, cmd2.cmdInfoType);
 }
+
+Test(Command, serialasation_empty_id) {
+	Plazza::Command cmd1;
+
+	cmd1.cmdFileName = "";
+	cmd1.cmdId = 0;
+	cmd1.cmdInfoType = Plazza::Information::EMAIL_ADDRESS;
+
+	std::stringstream ioss;
+	ioss << cmd1;
+
+	Plazza::Command cmd2;
+	ioss >> cmd2;
+	cr_assert_eq(cmd1.cmdId, cmd2.cmdId);
+	cr_assert_str_eq(cmd1.cmdFileName.c_str(), cmd2.cmdFileName.c_str());
+	cr_assert_str_eq(cmd1.cmdFileName.c_str(), cmd2.cmdFileName.c_str());
+	cr_assert_eq(cmd1.cmdInfoType, cmd2.cmdInfoType);
+}
+
