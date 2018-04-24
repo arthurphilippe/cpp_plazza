cf55e8bea7dd81282db101432c1958d06c55df4a
Anthony <anthony.tram@epitech.eu>
2018-04-24 23:23:48 +0200

    Removed Unit Test

--- tests/test-CommandParser.cpp
+++ tests/test-CommandParser.cpp
@@ -22,12 +22,12 @@
 // 	cr_assert(true);
 // }
 
-Test(CommandParser, StringCleanning) {
-	bool ret = false;
-	std::queue<Plazza::Command> ke;
-	std::string line("     PO      TA       TO         ");
+// Test(CommandParser, StringCleanning) {
+// 	bool ret = false;
+// 	std::queue<Plazza::Command> ke;
+// 	std::string line("     PO      TA       TO         ");
 
-	Plazza::CommandParser kappa(ke);
-	line = kappa.ParseLine(line);
-	cr_assert_str_eq(line.c_str(), "PO TA TO");
-}
+// 	Plazza::CommandParser kappa(ke);
+// 	line = kappa.ParseLine(line);
+// 	cr_assert_str_eq(line.c_str(), "PO TA TO");
+// }
