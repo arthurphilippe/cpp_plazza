2774a930251b2b2e573db78a021bde8ae2801261
Arthur C. Philippe <arthur.philippe@epitech.eu>
2018-05-02 10:21:40 +0200

    [#23] started logger

---
+++ include/master/Logger.hpp
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2018
+** cpp_plazza
+** File description:
+** Logger
+*/
+
+#ifndef LOGGER_HPP_
+	#define LOGGER_HPP_
+
+namespace plazza::master {
+	class Logger;
+}
+
+class plazza::master::Logger {
+public:
+	Logger();
+	~Logger();
+
+protected:
+private:
+};
+
+#endif /* !LOGGER_HPP_ */
---
+++ src/master/Logger.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2018
+** cpp_plazza
+** File description:
+** Logger
+*/
+
+#include "master/Logger.hpp"
+
+using plazza::master::Logger;
+
+Logger::Logger()
+{}
+
+Logger::~Logger()
+{}
